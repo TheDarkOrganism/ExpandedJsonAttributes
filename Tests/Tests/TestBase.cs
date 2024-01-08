63af87955cd8133d194da02b22e87fdd81d8d114
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2024-01-07 18:45:01 -0800

    Added Test Logic
    Added the main logic for the tests.

---
+++ Tests/Tests/TestBase.cs
@@ -0,0 +1,91 @@
+﻿namespace Tests.Tests
+{
+	public abstract class TestBase<TAttribute, TModel> : IClassFixture<TestConfig<TAttribute>>
+		where TAttribute : notnull, ValidationAttribute
+		where TModel : notnull
+	{
+		private readonly TestConfig<TAttribute> _config;
+
+		private readonly JsonSerializerOptions _simpleOptions;
+
+		private readonly JsonSerializerOptions _complexOptions;
+
+		protected internal virtual string SampleFolderPath { get; } = "Samples";
+
+		protected internal TestBase(TestConfig<TAttribute> config)
+		{
+			_config = config;
+
+			_simpleOptions = TestConfig<TAttribute>.GetSerializerOptions(GetModifier());
+
+			_complexOptions = TestConfig<TAttribute>.GetSerializerOptions(GetPropertyModifier());
+		}
+
+		protected internal abstract Action<JsonTypeInfo> GetModifier();
+
+		protected internal abstract Action<JsonTypeInfo, bool> GetPropertyModifier();
+
+		private async Task<TModel?> GetModel(string name, JsonSerializerOptions options)
+		{
+			return JsonSerializer.Deserialize<TModel>(await File.ReadAllTextAsync(Path.Combine(SampleFolderPath, _config.Name, $"{name}.json")), options);
+		}
+
+		protected internal async Task HandleBadAsync(JsonSerializerOptions options)
+		{
+			_ = await Assert.ThrowsAsync<ExpandedAttributeException>(async () => Assert.NotNull(await GetModel("Bad", options)));
+		}
+
+		protected internal async Task HandleGoodAsync(JsonSerializerOptions options)
+		{
+			Assert.NotNull(await GetModel("Good", options));
+		}
+
+		[Fact]
+		public async Task Bad()
+		{
+			await HandleBadAsync(_simpleOptions);
+		}
+
+		[Fact]
+		public async Task BadGeneric()
+		{
+			await HandleBadAsync(_config.GenericOptions);
+		}
+
+		[Fact]
+		public async Task Good()
+		{
+			await HandleGoodAsync(_simpleOptions);
+		}
+
+		[Fact]
+		public async Task GoodGeneric()
+		{
+			await HandleGoodAsync(_config.GenericOptions);
+		}
+
+		[Fact]
+		public async Task BadProperties()
+		{
+			await HandleBadAsync(_complexOptions);
+		}
+
+		[Fact]
+		public async Task BadGenericProperties()
+		{
+			await HandleBadAsync(_config.GenericPropertyOptions);
+		}
+
+		[Fact]
+		public async Task GoodProperties()
+		{
+			await HandleGoodAsync(_complexOptions);
+		}
+
+		[Fact]
+		public async Task GoodGenericProperties()
+		{
+			await HandleGoodAsync(_config.GenericPropertyOptions);
+		}
+	}
+}
---
+++ Tests/Tests/TestConfig.cs
@@ -0,0 +1,40 @@
+﻿namespace Tests.Tests
+{
+	public sealed class TestConfig<TAttribute>
+		where TAttribute : notnull, ValidationAttribute
+	{
+		internal string Name { get; }
+
+		internal JsonSerializerOptions GenericOptions { get; }
+
+		internal JsonSerializerOptions GenericPropertyOptions { get; }
+
+		internal static JsonSerializerOptions GetSerializerOptions(Action<JsonTypeInfo> modifier)
+		{
+			return new()
+			{
+				TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+				{
+					Modifiers =
+					{
+						modifier
+					}
+				}
+			};
+		}
+
+		internal static JsonSerializerOptions GetSerializerOptions(Action<JsonTypeInfo, bool> modifier)
+		{
+			return GetSerializerOptions(typeInfo => modifier(typeInfo, true));
+		}
+
+		public TestConfig()
+		{
+			Name = typeof(TAttribute).Name.Replace(nameof(Attribute), string.Empty);
+
+			GenericOptions = GetSerializerOptions(typeInfo => JsonModifiers.ValidateGeneric<TAttribute>(typeInfo));
+
+			GenericPropertyOptions = GetSerializerOptions((typeInfo, resolvePropertyNames) => JsonModifiers.ValidateGeneric<TAttribute>(typeInfo, resolvePropertyNames));
+		}
+	}
+}
